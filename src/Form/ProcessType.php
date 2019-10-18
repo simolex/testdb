71ac4530f5b59e2578ab1ccd396cad2fcfb57458
simolex <simolex@yandex.ru>
2019-10-18 16:07:11 +0300

    w20191018

--- src/Form/ProcessType.php
+++ src/Form/ProcessType.php
@@ -7,7 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class TaskType extends AbstractType
+class ProcessType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
@@ -26,4 +26,8 @@ class TaskType extends AbstractType
             'data_class' => NormProcess::class,
         ]);
     }
+
+    /**
+     * {@inheritdoc}
+     */
 }
